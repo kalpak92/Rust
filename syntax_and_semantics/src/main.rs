e9e61c63641f7ba5132a482394c8717282f21fc9
kalpak92 <kals9seals@gmail.com>
2023-09-20 00:12:38 -0700

    rust variables

--- syntax_and_semantics/src/main.rs
+++ syntax_and_semantics/src/main.rs
@@ -1,4 +1,12 @@
 #![allow(unused_variables)]
 fn main() {
-let (x, y) = (1, 2);
+    let x: i32 = 8;
+    {
+        println!("{}", x); // Prints "8".
+        let x = 12;
+        println!("{}", x); // Prints "12".
+    }
+    println!("{}", x); // Prints "8".
+    let x =  42;
+    println!("{}", x); // Prints "42".
 }
